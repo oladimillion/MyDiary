7196401cad44e6287f0a18b483dc9a763c4881ea
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2018-07-24 00:46:38 +0100

    add middleware to handle request to non-existing routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,4 +9,16 @@ const _path = "/api/v1";
 route.use(_path + "/auth", authRoute);
 route.use(_path + "/entries", diaryRoute);
 
+route.use((req, res) => {
+	res.type('text/json');
+	res.status(404);
+	return res.status(404).json({error: 'route does not exist'});
+});
+
+route.use((err, req, res, next) => {
+	console.log(err.stack);
+	res.status(500);
+	return res.status(500).json({error: 'Server error'});
+});
+
 export default route;
