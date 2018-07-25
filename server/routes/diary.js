96157e013f63a0a707ce3ea67b0f03fe337a99c4
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2018-07-26 00:43:06 +0100

    secure diary routes and add validation middleware

--- server/routes/diary.js
+++ server/routes/diary.js
@@ -1,14 +1,31 @@
 import express from "express";
-import { authenticate } from "../middlewares/authenticate";
+import { Private } from "../middlewares/private";
+import { DiaryMiddleware } from "../middlewares/diary";
 import diary from "../controllers/diary";
 
 const route = express.Router();
 
-route.get('/', diary.getEntries.bind(diary));
-route.get('/:id', diary.getEntry.bind(diary));
-route.post('/', diary.createEntry.bind(diary));
-route.put('/:id', diary.updateEntry.bind(diary));
+route.get('/', 
+  Private,
+  diary.getEntries.bind(diary)
+);
 
+route.get('/:id', 
+  Private,
+  diary.getEntry.bind(diary),
+);
+
+route.post('/', 
+  Private,
+  DiaryMiddleware,
+  diary.createEntry.bind(diary),
+);
+
+route.put('/:id', 
+  Private,
+  DiaryMiddleware,
+  diary.updateEntry.bind(diary),
+);
 
 export default route;
 
