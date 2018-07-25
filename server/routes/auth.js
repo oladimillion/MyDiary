8b2851409f24d897dbfa5612a21665f5b2ee6231
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2018-07-25 20:41:39 +0100

    add middlewares to auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,21 @@
 import express from "express";
 import auth from "../controllers/auth";
+import { 
+  SignupMiddleware, 
+  LoginMiddleware,
+} from "../middlewares/auth";
 
 const route = express.Router();
 
-route.post('/login', auth.login.bind(auth));
-route.post('/signup', auth.signup.bind(auth));
+route.post('/login', 
+  LoginMiddleware,
+  auth.login.bind(auth)
+);
+
+route.post('/signup', 
+  SignupMiddleware,
+  auth.signup.bind(auth)
+);
 
 
 export default route;
